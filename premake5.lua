c89e9bd2832aeb8bf5e62d6f9bef6333f347af0b
Charlie <charlie.challe@live.se>
2019-04-05 00:27:12 +0200

    Premake changes

--- premake5.lua
+++ premake5.lua
@@ -1,97 +1,107 @@
 -- PREMAKE FOR GLFW
 
 project "glfw"
-    kind "StaticLib"
-    language "C"
-    
-	targetdir ("bin/" .. output_dir .. "/%{prj.name}")
-    objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
+  kind "StaticLib"
+  language "C"
 
-	files
-	{
-        "include/glfw/glfw3.h",
-        "include/glfw/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+  targetdir ("bin/" .. output_dir .. "/%{prj.name}")
+  objdir ("bin-int/" .. output_dir .. "/%{prj.name}")
+
+  files
+  {
+    "include/glfw/glfw3.h",
+    "include/glfw/glfw3native.h",
+    "src/glfw_config.h",
+    "src/context.c",
+    "src/init.c",
+    "src/input.c",
+    "src/monitor.c",
+    "src/vulkan.c",
+    "src/window.c"
+  }
+
+  filter "system:linux"
+    systemversion "latest"
+    staticruntime "on"
+    pic "on"
+
+    files
+    {
+      "src/x11_init.c",
+      "src/x11_monitor.c",
+      "src/x11_window.c",
+      "src/xkb_unicode.c",
+      "src/posix_time.c",
+      "src/posix_thread.c",
+      "src/glx_context.c",
+      "src/egl_context.c",
+      "src/osmesa_context.c",
+      "src/linux_joystick.c"
     }
-    
-    filter "system:linux"
-        systemversion "latest"
-        staticruntime "On"
-        pic "On"
-        
-        files
-        {
-            "src/x11_init.c",
-            "src/x11_monitor.c",
-            "src/x11_window.c",
-            "src/xkb_unicode.c",
-            "src/posix_time.c",
-            "src/posix_thread.c",
-            "src/glx_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c",
-            "src/linux_joystick.c"
-        }
 
-        defines
-        {
-            "_GLFW_X11"
-        }       
+    defines
+    {
+      "_GLFW_X11"
+    }       
 
-	filter "system:windows"
-        systemversion "latest"
-        staticruntime "On"
-        
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
-        }
+  filter "system:windows"
+    systemversion "latest"
+    staticruntime "on"
 
-		defines 
-		{ 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-        }
-        
-        filter "system:macosx"
-        systemversion "latest"
-        staticruntime "On"
-        
-        files
-        {
-            "src/cocoa_init.m",
-            "src/cocoa_time.c",
-            "src/cocoa_window.m",
-            "src/cocoa_monitor.m",
-            "src/cocoa_joystick.m",
-            "src/cocoa_joystick.h",
-            "src/cocoa_platform.h",
-            "src/egl_context.c",
-            "src/wgl_context.c",
-            "src/glx_context.c",
-            "src/egl_context.h",
-            "src/wgl_context.h",
-            "src/glx_context.h"
-        }
+    files
+    {
+      "src/win32_init.c",
+      "src/win32_joystick.c",
+      "src/win32_monitor.c",
+      "src/win32_time.c",
+      "src/win32_thread.c",
+      "src/win32_window.c",
+      "src/wgl_context.c",
+      "src/egl_context.c",
+      "src/osmesa_context.c"
+    }
 
-        defines
-        {
-            "_GLFW_COCOA"
-        }      
+    defines 
+    { 
+      "_GLFW_WIN32",
+      "_CRT_SECURE_NO_WARNINGS"
+    }
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+  filter "system:macosx"
+    systemversion "latest"
+    staticruntime "on"
+
+    files
+    {
+      "src/cocoa_init.m",
+      "src/cocoa_time.c",
+      "src/cocoa_window.m",
+      "src/cocoa_monitor.m",
+      "src/cocoa_joystick.m",
+      "src/cocoa_joystick.h",
+      "src/cocoa_platform.h",
+      "src/egl_context.c",
+      "src/wgl_context.c",
+      "src/glx_context.c",
+      "src/egl_context.h",
+      "src/wgl_context.h",
+      "src/glx_context.h"
+    }
+
+    defines
+    {
+      "_GLFW_COCOA"
+    }
+
+  filter "configurations:Debug*"
+    runtime "Debug"
+    symbols "on"
+
+  filter "configurations:Release*"
+    runtime "Release"
+    symbols "on"
+    optimize "on"
+
+  filter "configurations:Dist*"
+    runtime "Release"
+    optimize "on"
