b6ec517a5d3499252b81e52f483fff45e095951e
Charlie Malmqvist <charlie.challe@live.se>
2020-04-28 17:39:38 +0200

    Premake optimization release & dist

--- premake5.lua
+++ premake5.lua
@@ -94,14 +94,19 @@ project "glfw"
     }
 
   filter "configurations:Debug*"
+    defines "WZ_CONFIG_DEBUG"
     runtime "Debug"
-    symbols "on"
+    symbols "On"
+    optimize "Off"
 
   filter "configurations:Release*"
+    defines "WZ_CONFIG_RELEASE"
     runtime "Release"
-    symbols "on"
-    optimize "on"
+    symbols "On"
+    optimize "On"
 
   filter "configurations:Dist*"
+    defines { "WZ_CONFIG_DIST", "WZ_DISABLE_ASSERTS" }
     runtime "Release"
-    optimize "on"
+    symbols "Off"
+    optimize "Speed"
